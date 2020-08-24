05073de8a1b4ac9a26b17e33444cd605ae4866f8
BonhommeG <BonhommeG@users.noreply.github.com>
2020-08-24 20:52:50 +0500

    config.json: notificationChannel <string> and watchTextChannels <string[]> params

--- index.ts
+++ index.ts
@@ -9,15 +9,19 @@ bot.login(config.token);
 
 bot.on('voiceStateUpdate', (oldState, newState) => {
     const userName = newState.member.displayName;
-    const textChannel = newState.guild.channels.cache.find(channel => channel.name === config.textChannel) as TextChannel;
+    const notificationChannel = newState.guild.channels.cache.find(channel => channel.name === config.notificationChannel) as TextChannel;
 
     if ((oldState.channelID !== newState.channelID) && newState.channel) {
         const channelName = newState.channel.name;
-        textChannel.send(`${userName} зашёл в войс-чат (${channelName}).`);
+        notificationChannel.send(`${userName} зашёл в войс-чат (${channelName}).`);
     }
 });
 
 bot.on('message', (message) => {
+    if (!config.watchTextChannels.includes((message.channel as TextChannel).name)) {
+        return;
+    }
+
     const command = helpers.getCommandFromString(message.toString());
     if (command) {
         const cmdFunction = commands[command];
